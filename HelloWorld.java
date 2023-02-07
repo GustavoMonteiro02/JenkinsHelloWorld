09ce48bd8ecd92b77406f0578b7f7a71ce79b846
Gustavo Monteiro <gdmm20032002@gmail.com>
2023-02-07 16:07:16 -0100

    Webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
   public static void main (String[] args){
-    System.out.println("---changes-----");
+    System.out.println("---Webhook trigered by jenkins-----");
   }
 }
